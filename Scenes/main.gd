3bbd1807cdc43cddfec1edf8f7ea5f958d8461c1
kacperszo <kacperszot1@gmail.com>
2024-12-14 23:45:53 +0100

    Museum Scene

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -5,7 +5,7 @@ extends Node2D
 func _ready() -> void:
 	await get_tree().process_frame
 	var scene_a = load("res://Scenes/sceneA.tscn").instantiate()
-	var scene_b = load("res://Scenes/sceneB.tscn").instantiate()
+	var scene_b = load("res://Scenes/Museum.tscn").instantiate()
 	#call_deferred("setup_scenes", scene_a, scene_b)
 	setup_scenes(scene_a, scene_b)
 	#pass # Replace with function body.
