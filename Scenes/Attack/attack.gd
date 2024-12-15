4f19f73c92ec151d0a73e52d47408d39e2594053
Kinga Bunkowska <tcx7@o2.pl>
2024-12-15 05:45:34 +0100

    Visually appealing club

--- Scenes/Attack/attack.gd
+++ Scenes/Attack/attack.gd
@@ -3,26 +3,35 @@ extends Node2D
 @export var duration:float = 0.5
 @export var rotation_speed:int = 135 * 2
 
-var player:CharacterBody2D
+var player_sprite:AnimatedSprite2D
 
 var time_left:float = duration
 
 func _ready():
-	player = get_parent()
+	var player = get_parent()
+	
+	for child in player.get_children():
+		if child != self:  # Pomijamy sam węzeł (bo to nie rodzeństwo)
+			if child.name == "AnimatedSprite2D":
+				player_sprite = child  # Znaleziono rodzeństwo
+				break
+	
 	self.rotation = -45
 	self.position = Vector2(0, 0)
 
 func _process(delta):
+	if player_sprite == null:
+		push_error("Player sprite is null")
 	time_left -= delta
-	#self.position = player.position
-	rotation += deg_to_rad(rotation_speed * delta)
+	
+	if not player_sprite.flip_h:
+		rotation -= deg_to_rad(rotation_speed * delta)
+	else:
+	
+		rotation += deg_to_rad(rotation_speed * delta)
 	
 	if time_left <= 0:
 		queue_free()
-
-
-func _on_area_2d_body_exited(body):
-	pass # Replace with function body.
 
 
 func _on_area_2d_body_entered(body):
