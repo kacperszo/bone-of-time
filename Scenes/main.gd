36edff05d249a2fcae7270a4418fafc64bfe86bb
kacperszo <kacperszot1@gmail.com>
2024-12-15 02:49:19 +0100

    Jurassic Map

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -4,7 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	await get_tree().process_frame
-	var scene_a = load("res://Scenes/sceneA.tscn").instantiate()
+	var scene_a = load("res://Scenes/Jurasic.tscn").instantiate()
 	var scene_b = load("res://Scenes/Museum.tscn").instantiate()
 	#call_deferred("setup_scenes", scene_a, scene_b)
 	setup_scenes(scene_a, scene_b)
